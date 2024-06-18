3156ba09005c49f50064663ccdf444ce69f77cb3
aminupy <eskandari.m.amin@gmail.com>
2024-06-18 23:27:23 +0330

    -- formatted bat script

--- deployment/deploy.bat
+++ deployment/deploy.bat
@@ -1,7 +1,13 @@
 @echo off
 REM Deploy services using Docker Compose
 
-docker compose -f docker-compose.yml -f ..\infra\api-gateway\docker-compose.yml -f ..\services\iam-service\docker-compose.yml -f ..\services\media-service\docker-compose.yml -f ..\services\ocr-service\docker-compose.yml up -d --build
+REM Combine Docker Compose files and build the services
+docker compose -f docker-compose.yml ^
+               -f ..\infra\api-gateway\docker-compose.yml ^
+               -f ..\services\iam-service\docker-compose.yml ^
+               -f ..\services\media-service\docker-compose.yml ^
+               -f ..\services\ocr-service\docker-compose.yml ^
+               up -d --build
 
 REM Check the exit code of the last command and print result
 if %ERRORLEVEL% EQU 0 (
