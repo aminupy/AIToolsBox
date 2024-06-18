5d09f74e2ad7965864d026361e8a948be1e0f238
aminupy <eskandari.m.amin@gmail.com>
2024-06-18 23:20:16 +0330

    Added Windows deployment Script

---
+++ deployment/deploy.bat
@@ -0,0 +1,16 @@
+@echo off
+REM Deploy services using Docker Compose
+
+docker compose -f docker-compose.yml -f ..\infra\api-gateway\docker-compose.yml -f ..\services\iam-service\docker-compose.yml -f ..\services\media-service\docker-compose.yml -f ..\services\ocr-service\docker-compose.yml up -d --build
+
+REM Check the exit code of the last command and print result
+if %ERRORLEVEL% EQU 0 (
+    echo Deployment succeeded.
+) else (
+    echo Deployment failed with error code %ERRORLEVEL%.
+)
+
+REM Wait for user input before closing
+echo.
+echo Press any key to exit...
+pause >nul
