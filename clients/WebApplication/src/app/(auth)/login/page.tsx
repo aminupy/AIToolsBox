ceed899d689fb36ae87909e7aa9dfefdb3222e5a
Morteza Pourrmazan <pourramzanmorteza@gmail.com>
2024-07-31 23:54:35 +0330

    Update signup form

---
+++ clients/WebApplication/src/app/(auth)/login/page.tsx
@@ -0,0 +1,7 @@
+export default function Login() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      Login Page
+    </main>
+  );
+}
--- clients/WebApplication/src/app/(auth)/signup/page.tsx
+++ clients/WebApplication/src/app/(auth)/signup/page.tsx
@@ -1,10 +1,51 @@
+"use client";
 import Image from "next/image";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
+import { SubmitHandler, useForm } from "react-hook-form";
+import { useState } from "react";
+import { MdVisibility, MdVisibilityOff } from "react-icons/md";
+import { useRouter } from "next/navigation";
+
+type Inputs = {
+  email: string;
+  password: string;
+};
 
 export default function SignUp() {
+  const router = useRouter();
+  const [isEmailValid, setIsEmailValid] = useState(false);
+  const [isVisible, setIsVisible] = useState(false);
+  const handleVisibility = () => setIsVisible(!isVisible);
+  const {
+    register,
+    handleSubmit,
+    setError,
+    reset,
+    formState: { errors },
+  } = useForm<Inputs>();
+
+  const handleEmailChange = () => {
+    setIsEmailValid(false);
+    reset({ password: "" });
+  };
+
+  const onSubmit: SubmitHandler<Inputs> = (data: any) => {
+    const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!emailPattern.test(data.email)) {
+      setError("email", {
+        type: "manual",
+        message: "Invalid email format",
+      });
+      setIsEmailValid(false);
+      return;
+    }
+    setIsEmailValid(true);
+    // Handle valid form submission
+    console.log("Form submitted successfully", data);
+  };
   return (
-    <main className="flex  mg:flex-row h-screen justify-center items-center mg:pb-16">
+    <main className="flex mg:flex-row h-screen justify-center items-center mg:pb-16">
       <div className="h-full mg:h-[80%] w-full mg:w-9/12 bg-gradient-180 mg:bg-gradient-360 shadow-md mg:rounded-5xl mg:rounded-r-6xl">
         <div className="h-full flex flex-col mg:flex-row justify-between">
           <div className="text-white flex flex-col items-center h-[60%] mg:w-[45%] pt-10 mg:pt-28">
@@ -22,28 +63,75 @@ export default function SignUp() {
             <div className="pt-7 hidden mg:block">
               <Image src="/Logo.png" width={70} height={70} alt="Logo" />
             </div>
-            <div className="flex flex-col items-center w-80 sm:w-[30rem] mg:w-80 lg:w-96 2xl:w-[28rem] h-full my-8">
-              <h2 className="text-3xl sm:text-4xl mg:text-2xl xl:text-3xl 2xl:text-4xl font-bold">
+            <div className="flex flex-col items-center w-80 sm:w-[30rem] mg:w-80 lg:w-[23rem] 2xl:w-[28rem] h-full my-8">
+              <h2 className="text-3xl sm:text-4xl mg:text-3xl 2xl:text-4xl font-bold">
                 Create an account
               </h2>
-              <div className="w-full flex flex-col gap-5">
+              <form
+                className="w-full flex flex-col gap-5"
+                onSubmit={handleSubmit(onSubmit)}
+              >
                 <div className="flex flex-col gap-3 mt-9">
-                  <Input
-                    type="email"
-                    placeholder="Email Address"
-                    className="h-14"
-                  />
-                  <Input
-                    type="password"
-                    placeholder="Password"
-                    className="h-14 "
-                  />
+                  <div>
+                    <div className="relative">
+                      <Input
+                        type="email"
+                        placeholder="Email Address"
+                        className={`h-14 ${
+                          errors.email ? "border-red-600" : ""
+                        }`}
+                        readOnly={isEmailValid}
+                        {...register("email")}
+                      />
+                      {errors.email && (
+                        <p className="font-semibold text-sm text-red-500 pl-2 pt-1">
+                          {errors.email.message}
+                        </p>
+                      )}
+                      {isEmailValid && (
+                        <div
+                          onClick={handleEmailChange}
+                          className="absolute top-0 right-0 text-blue mt-4 mr-5 cursor-pointer"
+                        >
+                          Edit
+                        </div>
+                      )}
+                    </div>
+                  </div>
+
+                  {isEmailValid && (
+                    <div>
+                      <div className="relative">
+                        <Input
+                          type={isVisible ? "text" : "password"}
+                          placeholder="Password"
+                          className="h-14"
+                          {...register("password")}
+                        />
+                        <div
+                          className="absolute top-1 right-0 mt-4 mr-5 cursor-pointer"
+                          onClick={handleVisibility}
+                        >
+                          {isVisible ? (
+                            <MdVisibilityOff size={20} />
+                          ) : (
+                            <MdVisibility size={20} />
+                          )}
+                        </div>
+                      </div>
+                    </div>
+                  )}
                 </div>
 
                 <Button className="h-14 w-full bg-gradient-90">Continue</Button>
                 <p className="font-semibold text-sm mx-auto">
                   Already have an account?{" "}
-                  <span className="text-blue ">Login</span>
+                  <span
+                    onClick={() => router.push("/login")}
+                    className="text-blue cursor-pointer ml-1"
+                  >
+                    Login
+                  </span>
                 </p>
                 <div className="relative flex items-center w-full my-4">
                   <div className="flex-grow border-t border-gray-400"></div>
@@ -61,7 +149,7 @@ export default function SignUp() {
                     Continue with Google
                   </Button>
                 </div>
-              </div>
+              </form>
             </div>
           </div>
         </div>
